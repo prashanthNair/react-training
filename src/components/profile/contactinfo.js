0b51f404224b3366d68055199456b47413c80582
Vishnu Kumar <vishnukumarks@gmail.com>
2022-07-19 21:28:28 +0530

    Profile page modified

--- src/components/profile/contactinfo.js
+++ src/components/profile/contactinfo.js
@@ -1,34 +1,13 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import CustomTextField from '../customtext';
 import { Grid } from '@mui/material';
+import * as React from 'react';
+import CustomTextField from '../customtext';
+
 
 export default function Contactinfo() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
 
   return (
     <div>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Contact Info
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid container>
+      <Grid container>
           <Grid item xs={4}><h3>Contact Info</h3></Grid>
           </Grid>
           <Grid container>
@@ -39,8 +18,6 @@ export default function Contactinfo() {
           <Grid item xs={6}><CustomTextField text="Email Address"></CustomTextField></Grid>
           <Grid item xs={6}><CustomTextField text="Preffered Language"></CustomTextField></Grid>
           </Grid>
-        </AccordionDetails>
-      </Accordion>
     </div>
   );
 }
---
+++ src/components/profile/profileDetails.js
@@ -0,0 +1,35 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import Contactinfo from './contactinfo';
+
+export default function ProfileDetails() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Contact Info
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Contactinfo></Contactinfo>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
